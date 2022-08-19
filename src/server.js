fd6c4411b7eaddf61698b7f1a53cc903c1ebbf73
mochamadwiby <masertons@outlook.com>
2022-08-19 09:12:06 +0700

    bugfix/server.js

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const corsOptions = {
+  origin: 'http://localhost:8081',
+};
+const db = require("./app/models");
+db.sequelize.sync()
+  .then(() => {
+    console.log("Synced db.");
+  })
+  .catch((err) => {
+    console.log("Failed to sync db: " + err.message);
+  });
+app.use(cors(corsOptions));
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+// simple route
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to erton application.' });
+});
+require("./src/app/routes/turorial.routes")(app);
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
+});
